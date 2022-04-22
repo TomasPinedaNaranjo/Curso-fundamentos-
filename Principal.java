da718af907a583a9def9677f20e3177d4c300dee
TomasPinedaNaranjo <104210520+TomasPinedaNaranjo@users.noreply.github.com>
2022-04-22 16:11:52 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Principal{
-    Scanner scan2 = new Scanner(System.in);
+   
     // MAIN 
     public static void main (String[]args){
         Principal.mostrarMenu();
